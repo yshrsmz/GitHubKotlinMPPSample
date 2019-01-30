8de2b88e3128b3ba5d42f3bcd0fb3c3acead88e2
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-01-30 19:37:01 +0900

    :construction: android part

--- app/src/main/java/com/codingfeline/app/MainActivity.kt
+++ app/src/main/java/com/codingfeline/app/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.codingfeline.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.codingfeline.githubdata.getGitHubRepository
+import com.squareup.sqldelight.runtime.rx.asObservable
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val repository = getGitHubRepository(applicationContext)
+
+    val login = "yshrsmz"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repository.observeUser(login).asObservable()
+            .map { it.executeAsOneOrNull() }
+            .subscribe { println("user: $it") }
+
+
+        GlobalScope.launch {
+            repository.fetchUser(login)
+        }
     }
 }
